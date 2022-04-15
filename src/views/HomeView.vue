d767d2d20440072aa879d81184815826e198ac04
Y2theK <yeyintkyaw300@gmail.com>
2022-04-15 04:16:16 -0700

    composition api basic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>home</h3>
+    <p ref="p">name : {{ name }} , age : {{ age }}</p>
+
+    <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from "@vue/reactivity";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  // created() {
+  //   console.log("created");
+  // },
+  // mounted() {
+  //   console.log("mounted");
+  // },
+
+  setup() {
+    console.log("setup");
+    let name = "mario";
+    let age = 30;
+    //null is value of ref
+    //const p mean ref is const but value can reactive
+    const p = ref(null);
+    console.log(p.value);
+    // return { name: name, age: age };
+    //property shorthand
+    const handleClick = () => {
+      console.log("you click me");
+      //not change because it is compostion  not reactive by default
+      name = "gigi";
+      console.log(p.value);
+      p.value.textContent = "hello reactive";
+    };
+    return { name, age, handleClick, p };
+  },
+};
 </script>
