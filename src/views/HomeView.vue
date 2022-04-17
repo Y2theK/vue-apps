4cb6c5f400a5358df09b15361468d329889b7c74
Y2theK <yeyintkyaw300@gmail.com>
2022-04-17 14:40:38 -0700

    lifecycle hooks in composition api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="home">
-    <h3>Props</h3>
-    <div v-for="person in persons" :key="person.name">
-      <detail-view :person="person"></detail-view>
+    <h3>LifeCycle hooks</h3>
+    <div v-if="showPerson">
+      <div v-for="person in persons" :key="person.name">
+        <detail-view :person="person"></detail-view>
+      </div>
     </div>
+    <button v-on:click="showPerson = !showPerson">Toggle Persons</button>
   </div>
 </template>
 
 <script>
 import { ref } from "@vue/reactivity";
 import DetailView from "./DetailView.vue";
+import { onMounted, onUnmounted, onUpdated } from "@vue/runtime-core";
 export default {
   name: "HomeView",
   components: { DetailView },
@@ -26,7 +30,23 @@ export default {
       { name: "fee", age: 21, gender: "female", bio: "ispam" },
     ]);
 
-    return { persons };
+    //life cycle hooks in composition api()
+    onMounted(() => {
+      console.log("mounted");
+    });
+    onUnmounted(() => {
+      console.log("unmounted");
+    });
+    onUpdated(() => {
+      console.log("updated");
+    });
+
+    const showPerson = ref(true);
+    return { persons, showPerson };
   },
+  //life cycle hook in option api
+  mounted() {},
+  updated() {},
+  unmounted() {},
 };
 </script>
